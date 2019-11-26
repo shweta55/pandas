1e26e012e706802d9bc8fa4e99e8dafc4b14c288
shweta55 <ss191619@gmail.com>
2019-11-26 18:47:38 +0530

    Update setup_env.sh

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -171,10 +171,10 @@ conda list
 # Install DB for Linux
 #if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
  # echo "installing dbs"
- # sudo systemctl start mysql
- # sudo service postgresql start 
- # mysql -e 'create database pandas_nosetest;'
- # psql -c 'create database pandas_nosetest;' -U postgres
+  sudo systemctl start mysql
+  sudo service postgresql start 
+  mysql -e 'create database pandas_nosetest;'
+  psql -c 'create database pandas_nosetest;' -U postgres
 #else
  #  echo "not using dbs on non-linux Travis builds or Azure Pipelines"
 #fi
